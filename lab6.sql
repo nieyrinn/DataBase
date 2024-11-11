2105af141f620368bee129388ccb6152f68f081f
nieyrinn <kken77701@gmail.com>
2024-11-11 12:23:33 +0500

    SQL lab6

--- lab6.sql
+++ lab6.sql
@@ -3,7 +3,7 @@ CREATE TABLE Locations (
                            location_id SERIAL PRIMARY KEY,
                            street_address VARCHAR(25),
                            postal_code VARCHAR(12),
-                           city VARCHAR(12),
+                           city VARCHAR(30),
                            state_province VARCHAR(12)
 );
 
@@ -23,6 +23,20 @@ CREATE TABLE Employees (
                            salary INTEGER,
                            department_id INTEGER REFERENCES Departments
 );
+
+INSERT INTO Locations (street_address, postal_code, city, state_province) VALUES
+                                                                              ('123 Street A', '54321', 'Almaty', 'Almaty '),
+                                                                              ('456 Street B', '67890', 'Astana', 'Akmola ');
+
+INSERT INTO Departments (department_name, budget, location_id) VALUES
+                                                                   ('Department_A', 100000, 1),
+                                                                   ('Department_B', 75000, 2);
+
+
+INSERT INTO Employees (first_name, last_name, email, phone_number, salary, department_id) VALUES
+                                                                                              ('Alice', 'Smith', 'alice.smith@company.com', '+77015551234', 55000, 1),
+                                                                                              ('Bob', 'Jones', 'bob.jones@company.com', '+77025552345', 75000, 2);
+
 SELECT Employees.first_name, Employees.last_name, Employees.department_id, Departments.department_name
 FROM Employees
          JOIN Departments ON Employees.department_id = Departments.department_id;
@@ -45,3 +59,7 @@ GROUP BY Departments.department_name;
 SELECT Employees.first_name, Employees.last_name, Employees.department_id, Departments.department_name
 FROM Employees
          LEFT JOIN Departments ON Employees.department_id = Departments.department_id;
+
+Select Employees.first_name, Employees.last_name, Employees.department_id from Employees
+Join Departments On Employees.department_id = Departments.department_id join Locations L on L.location_id = Departments.location_id
+where city in ('Almaty', 'Astana')
